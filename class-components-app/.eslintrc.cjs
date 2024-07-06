108e9c686cd16760fc7811939410e1bef80f4e56
Frost-704 <boolean.pg.me@gmail.com>
2024-07-06 18:37:25 +0300

    chore: Set up eslint-plugin-react-compiler

--- class-components-app/.eslintrc.cjs
+++ class-components-app/.eslintrc.cjs
@@ -8,11 +8,14 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react-compiler'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    "react-compiler/react-compiler": "error"
   },
 }
