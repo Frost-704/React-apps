f0b16798bb005ef6cb9fb010cbcdf405bbad83b3
Frost-704 <boolean.pg.me@gmail.com>
2024-07-08 02:02:21 +0300

    fix: delete stylelint

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
 import styles from './Results.module.scss'
 import { Man } from '../../api/api'
 
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, Component } from 'react'
+import { ChangeEvent, Component } from 'react'
 import styles from './Search.module.scss'
 
 interface SearchProps {
